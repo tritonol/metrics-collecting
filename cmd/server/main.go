fc71a2bd5461f31edfaa2001609a2d840cde70aa
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2023-08-06 12:13:54 +0300

    add chi

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/tritonol/metrics-collecting.git/internal/server/handlers/metrics/get"
 	"github.com/tritonol/metrics-collecting.git/internal/server/handlers/metrics/save"
 	"github.com/tritonol/metrics-collecting.git/internal/storage/memstorage"
 )
 
 func main() {
-	storage := memstorage.NewMemStorage()
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/update/", save.New(storage))
-
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", MetricRouter())
 	if err != nil {
 		panic(err)
 	}
+}
+
+func MetricRouter() chi.Router {
+	r := chi.NewRouter()
+	storage := memstorage.NewMemStorage()
+
+	r.Post("/update/{type}/{name}/{value}", save.New(storage))
+	r.Get("/value/{type}/{name}", get.Get(storage))
+	r.Get("/", get.MainPage(storage))
+	return r
 }
