f0b5da446fc96b452f3b08f6a347831289306666
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2023-08-23 17:39:55 +0300

    feat

--- internal/server/handlers/metrics/save/save.go
+++ internal/server/handlers/metrics/save/save.go
@@ -2,8 +2,12 @@ package save
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	jsonstructs "github.com/tritonol/metrics-collecting.git/internal/structs/JSON"
@@ -60,7 +64,29 @@ func NewJSON(metricSaver MetricSaver) http.HandlerFunc {
 		var metric jsonstructs.Metrics
 
 		if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
-			http.Error(w, "Invalid JSON string", http.StatusBadRequest)
+			var syntaxError *json.SyntaxError
+			var unmarshalTypeError *json.UnmarshalTypeError
+
+			switch {
+			case errors.As(err, &syntaxError):
+				msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+				http.Error(w, msg, http.StatusBadRequest)
+			case errors.Is(err, io.ErrUnexpectedEOF):
+				msg := "Request body contains badly-formed JSON"
+				http.Error(w, msg, http.StatusBadRequest)
+			case errors.As(err, &unmarshalTypeError):
+				msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+				http.Error(w, msg, http.StatusBadRequest)
+			case strings.HasPrefix(err.Error(), "json: unknown field "):
+				fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+				msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+				http.Error(w, msg, http.StatusBadRequest)
+			case errors.Is(err, io.EOF):
+				msg := "Request body must not be empty"
+				http.Error(w, msg, http.StatusBadRequest)
+			default:
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
 			return
 		}
 
