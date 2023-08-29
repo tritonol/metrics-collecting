ee708ef66578f157eecaf0ed195cea02c74957b2
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2023-08-29 12:10:36 +0300

    feat

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/tritonol/metrics-collecting.git/internal/backup"
 	"github.com/tritonol/metrics-collecting.git/internal/routes"
@@ -25,6 +28,19 @@ func main() {
 	}
 
 	go backup.SaveMetricsPeriodically(cfg.Backup.StoreInterval, cfg.Backup.FilePath, storage)
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-stopChan
+		err := backup.SaveMetricsToFile(cfg.Backup.FilePath, storage)
+		if err != nil {
+			logger.Error("Error save metric:", zap.Error(err))
+		}
+
+		os.Exit(0)
+	}()
 
 	err := http.ListenAndServe(cfg.Server.Address, routes.MetricRouter(storage, logger))
 	if err != nil {
