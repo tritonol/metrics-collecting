09c2717a7c2d9dc35b25fd21500562ec21760571
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2023-11-03 10:51:44 +0300

    add pgx

---
+++ internal/storage/pgstorage/pgstorage.go
@@ -0,0 +1,39 @@
+package pgstorage
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type postgres struct {
+	db *pgxpool.Pool
+}
+
+var (
+	pgInstance *postgres
+	pgOnce     sync.Once
+)
+
+func NewPg(ctx context.Context, connString string) (*postgres, error) {
+	pgOnce.Do(func() {
+		db, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			return fmt.Errorf("Unable to create connection pool: %w", err)
+		}
+
+		pgInstance = &postgres{db}
+	})
+
+	return pgInstance, nil
+}
+
+func (pg *postgres) Ping(ctx context.Context) error {
+	return pg.db.Ping(ctx)
+}
+
+func (pg *postgres) Close() {
+	pg.db.Close()
+}
