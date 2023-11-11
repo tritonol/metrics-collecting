5ffe836afd5cf03623138868c93fe600c65b698f
Кирилл Сабуров <kirillsaburov@192.168.1.4>
2023-11-11 10:54:05 +0300

    add new flag to config

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	Server HTTPServer
 	Backup Backup
+	Db	Db
 }
 
 type HTTPServer struct {
@@ -20,16 +21,25 @@ type Backup struct{
 	Restore bool
 }
 
+type Db struct {
+	ConnString string
+}
+
 func MustLoad() *Config {
 	var cfg Config
 	flag.StringVar(&cfg.Server.Address, "a", ":8080", "address to run server")
 	flag.StringVar(&cfg.Backup.FilePath, "f", "/tmp/metrics-db.json", "Path to backup file")
 	flag.BoolVar(&cfg.Backup.Restore, "r", true, "Load previously saved values from file")
 	flag.Int64Var(&cfg.Backup.StoreInterval, "i", 300, "Backup interval")
+	flag.StringVar(&cfg.Db.ConnString, "d", "", "Pg connection string")
 	flag.Parse()
 
 	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
 		cfg.Server.Address = envAddr
 	}
+	if envDbDSN := os.Getenv("DATABASE_DSN"); envDbDSN != "" {
+		cfg.Db.ConnString = envDbDSN
+	}
+
 	return &cfg
 }
