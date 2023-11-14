4bd68aaf49e08ba158e2066a208773d0deca1ec3
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2023-11-14 23:58:33 +0300

    feat

--- internal/agent/request/sendmetric.go
+++ internal/agent/request/sendmetric.go
@@ -2,8 +2,10 @@ package request
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -17,7 +19,7 @@ type MetricRequest interface {
 	CollectGauge() map[string]float64
 }
 
-func sendJSONMetrics(serverAddress, mtype, mname string, mvalue interface{}) {
+func sendJSONMetrics(serverAddress, mtype, mname string, mvalue interface{}) error {
 	url := fmt.Sprintf("%s/update/", serverAddress)
 
 	var body mj.Metrics
@@ -30,8 +32,7 @@ func sendJSONMetrics(serverAddress, mtype, mname string, mvalue interface{}) {
 	case "counter":
 		delta = mvalue.(int64)
 	default:
-		fmt.Println("Invalid metric type")
-		return
+		return fmt.Errorf("invalid metric type: %s", mtype)
 	}
 
 	body = mj.Metrics{
@@ -43,21 +44,40 @@ func sendJSONMetrics(serverAddress, mtype, mname string, mvalue interface{}) {
 
 	data, err := json.Marshal(body)
 	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return
+		return fmt.Errorf("error marshaling JSON: %w", err)
 	}
 
 	resp, err := http.Post(url, "application/json", bytes.NewReader(data))
 	if err != nil {
-		fmt.Println("Error requesting metrics:", err)
-		return
+		return fmt.Errorf("error requesting metrics: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Server returned non-200 status code:", resp.Status)
-		return
+		return fmt.Errorf("server returned non-200 status code: %s", resp.Status)
 	}
+
+	return nil
+}
+
+func retryableHTTPPost(ctx context.Context, url string, data *bytes.Buffer) (*http.Response, error) {
+	var resp *http.Response
+	var lastErr error
+
+	for retry := 0; retry < RetryCount; retry++ {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err() // Context canceled or deadline exceeded
+		default:
+			resp, lastErr = http.Post(url, "application/json", data)
+			if lastErr == nil && resp.StatusCode == http.StatusOK {
+				return resp, nil
+			}
+			time.Sleep(time.Second * time.Duration((retry+1)*2))
+		}
+	}
+
+	return resp, lastErr
 }
 
 func Send(metricRequest MetricRequest, serverAddress string) {
@@ -65,11 +85,15 @@ func Send(metricRequest MetricRequest, serverAddress string) {
 	counterMetrics := metricRequest.CollectCounter()
 
 	for metric, value := range gaugeMetrics {
-		sendJSONMetrics(serverAddress, "gauge", metric, value)
+		if err := sendJSONMetrics(serverAddress, "gauge", metric, value); err != nil {
+			log.Printf("Error sending gauge metric %s: %v", metric, err)
+		}
 	}
 
 	for metric, value := range counterMetrics {
-		sendJSONMetrics(serverAddress, "counter", metric, value)
+		if err := sendJSONMetrics(serverAddress, "counter", metric, value); err != nil {
+			log.Printf("Error sending counter metric %s: %v", metric, err)
+		}
 	}
 }
 
@@ -105,30 +129,22 @@ func SendBatch(metricRequest MetricRequest, serverAddress string) {
 	var buf bytes.Buffer
 	err := json.NewEncoder(&buf).Encode(metrics)
 	if err != nil {
-		fmt.Println("Error requesting metrics:", err)
+		log.Printf("Error encoding metrics: %v", err)
 		return
 	}
 
-	var resp *http.Response
-	var lastErr error
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	for retry := 0; retry < RetryCount; retry++ {
-		resp, lastErr = http.Post(url, "application/json", &buf)
-		if lastErr == nil && resp.StatusCode == http.StatusOK {
-			break
-		}
-		defer resp.Body.Close()
-
-		time.Sleep(time.Second * time.Duration((retry+1)*2))
-	}
-
-	if lastErr != nil {
-		fmt.Printf("Error sending batch metrics after %d retries: %s", RetryCount, lastErr)
+	resp, err := retryableHTTPPost(ctx, url, &buf)
+	if err != nil {
+		log.Printf("Error sending batch metrics after %d retries: %v", RetryCount, err)
 		return
 	}
+	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Server returned non-200 status code:", resp.Status)
+		log.Printf("Server returned non-200 status code: %s", resp.Status)
 		return
 	}
 }
